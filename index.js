14316e701432fd46e1c4df7fdbef5b4db3cc3d84
Israa Essmat <israaessmat172@gmail.com>
2024-08-13 01:41:48 +0300

    user model

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 const express = require('express');
-const podyParser = require('pody-parser');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+require('dotenv').config();
+require('./db')
+const PORT = 8000;
+
+app.use(bodyParser.json());
+
+app.get('/', (req,res) => {
+    res.json({
+        message: 'Task Manager API is working!'
+    })
+});
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}.`);
+})
