177e8ff939b88c1331ea3d4c713135e5bd8e4f9c
Israa Essmat <israaessmat172@gmail.com>
2024-08-21 01:16:17 +0300

    taskmodel

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const router = express.Router();
+const auth = require('../middlewares/auth');
+const Task = require("../models/Task");
 
-router.get("/", (req, res) => {
-  res.send("Task routes are working!");
+
+router.get("/test", auth, (req, res) => {
+  res.json({
+    message:"Task routes are working!",
+    user: req.user});
 });
 
 //CRUD tasks
+
 
 module.exports = router;
